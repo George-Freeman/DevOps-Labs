9d76ed0956c810e998eb87c9b4fe65b054d3082e
George-Freeman <george.freeman@gmail.com>
2026-09-23 14:09:09 +0100

    Complete MongoDB Docker integration

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -9,17 +9,28 @@ public class App
 {
     public static void main(String[] args)
     {
-        // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("localhost", 27000);
+        // Get MongoDB connection details from environment variables
+        // Defaults allow the application to run directly on the Mac
+        String mongoHost = System.getenv().getOrDefault("MONGO_HOST", "localhost");
+        int mongoPort = Integer.parseInt(
+                System.getenv().getOrDefault("MONGO_PORT", "27000")
+        );
+
+        // Connect to MongoDB
+        MongoClient mongoClient = new MongoClient(mongoHost, mongoPort);
+
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
+
         // Get a collection from the database
         MongoCollection<Document> collection = database.getCollection("test");
+
         // Create a document to store
         Document doc = new Document("name", "Kevin Sim")
                 .append("class", "DevOps")
                 .append("year", "2024")
                 .append("result", new Document("CW", 95).append("EX", 85));
+
         // Add document to collection
         collection.insertOne(doc);
 
