f5a97413772de1fab94d280a5518e7643dafd19d
George-Freeman <george.freeman@gmail.com>
2026-09-23 15:30:30 +0100

    Implement employee record lookup

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -3,9 +3,38 @@ package com.napier.sem;
 import java.sql.*;
 
 public class App {
+    // Connection to MySQL
+    private Connection con = null;
+
     public static void main(String[] args) {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+        Employee emp = a.getEmployee(255530);
+
+        if (emp != null) {
+            System.out.println("Employee Number: " + emp.emp_no);
+            System.out.println("Name: " + emp.first_name + " " + emp.last_name);
+            System.out.println("Title: " + emp.title);
+            System.out.println("Salary: " + emp.salary);
+            System.out.println("Department: " + emp.dept_name);
+            System.out.println("Manager: " + emp.manager);
+        }
+        else {
+            System.out.println("Employee not found");
+        }
+        // Disconnect from database
+        a.disconnect();
+    }
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect() {
         try {
-            // Load Database driver
+            // Load database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
         }
         catch (ClassNotFoundException e) {
@@ -13,15 +42,13 @@ public class App {
             System.exit(-1);
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
+        int retries = 10;
 
         for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
 
             try {
-                // Wait a bit for db to start
+                // Wait for database to start
                 Thread.sleep(30000);
 
                 // Connect to database
@@ -32,11 +59,6 @@ public class App {
                 );
 
                 System.out.println("Successfully connected");
-
-                // Wait a bit
-                Thread.sleep(10000);
-
-                // Exit for loop
                 break;
             }
             catch (SQLException sqle) {
@@ -50,15 +72,72 @@ public class App {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect() {
         if (con != null) {
             try {
-                // Close connection
                 con.close();
+                System.out.println("Connection closed");
             }
             catch (Exception e) {
                 System.out.println("Error closing connection to database");
             }
         }
+    }
+    /**
+     * Gets an employee from the database.
+     *
+     * @paramID Employee number
+     * @return Employee object, or null if not found
+     */
+    public Employee getEmployee(int ID) {
+        Employee emp = null;
+
+        try {
+            String sql =
+                    "SELECT e.emp_no, e.first_name, e.last_name, " +
+                            "t.title, s.salary, d.dept_name, " +
+                            "CONCAT(m.first_name, ' ', m.last_name) AS manager " +
+                            "FROM employees e " +
+                            "JOIN titles t ON e.emp_no = t.emp_no " +
+                            "AND t.to_date = '9999-01-01' " +
+                            "JOIN salaries s ON e.emp_no = s.emp_no " +
+                            "AND s.to_date = '9999-01-01' " +
+                            "JOIN dept_emp de ON e.emp_no = de.emp_no " +
+                            "AND de.to_date = '9999-01-01' " +
+                            "JOIN departments d ON de.dept_no = d.dept_no " +
+                            "JOIN dept_manager dm ON d.dept_no = dm.dept_no " +
+                            "AND dm.to_date = '9999-01-01' " +
+                            "JOIN employees m ON dm.emp_no = m.emp_no " +
+                            "WHERE e.emp_no = ?";
+
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt(1, ID);
+            ResultSet rset = stmt.executeQuery();
+
+            if (rset.next()) {
+                emp = new Employee();
+                emp.emp_no = rset.getInt("emp_no");
+                emp.first_name = rset.getString("first_name");
+                emp.last_name = rset.getString("last_name");
+                emp.title = rset.getString("title");
+                emp.salary = rset.getInt("salary");
+                emp.dept_name = rset.getString("dept_name");
+                emp.manager = rset.getString("manager");
+            }
+            rset.close();
+            stmt.close();
+        }
+
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+        }
+        return emp;
+
     }
 }
---
+++ src/main/java/com/napier/sem/Employee.java
@@ -0,0 +1,24 @@
+package com.napier.sem;
+
+public class Employee {
+    /*** Employee number*/
+    public int emp_no;
+
+    /*** Employee first name*/
+    public String first_name;
+
+    /*** Employee last name*/
+    public String last_name;
+
+    /***Employee job title*/
+    public String title;
+
+    /*** Employee salary*/
+    public int salary;
+
+    /*** Employee department*/
+    public String dept_name;
+
+    /*** Employee's manager*/
+    public String manager;
+}
